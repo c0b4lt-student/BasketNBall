bcdeeb4ad0812067a18bc579c8469462ea587686
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-05-24 17:42:03 +0000

    Adding random generator function, add fillbasket function, fillbasketS function

--- script/main.js
+++ script/main.js
@@ -1,11 +1,45 @@
 $(document).ready(() => {
-  let maxBallNumber = $(`#max-ball-number`);
-  let nbBasket = $(`#nb-basket`);
-  let basketCapacity = $(`#basket-capacity`);
-  let userBasketCapacity = $(`#user-basket-capacity`);
+  let answer = $(`#answer`);
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function fillBasket(capacity, maxBall) {
+    let load = getRandomInt(capacity + 1);
+    let basket = [];
+
+    for(let i = 0; i < load; i++) {
+      let ball = getRandomInt(maxBall) + 1;
+
+      if (basket.find(element => element === ball)) {
+        i--;
+      } else {
+        basket.push(ball);
+      }
+    }
+    basket.push('END');
+    return basket;
+  }
+  function fillBaskets(nbBasket, basketCapacity, maxBallNumber) {
+    let allBasket = [];
+    for(let i = 0; i < nbBasket; i++) {
+      allBasket.push(fillBasket(basketCapacity, maxBallNumber));
+    }
+    return allBasket;
+  }
 
   function runSim() {
-    alert(`${maxBallNumber.attr('value')}`);
+    answer.html('');
+    let maxBallNumber = parseInt($(`#max-ball-number`).val());
+    let nbBasket = parseInt($(`#nb-basket`).val());
+    let basketCapacity = parseInt($(`#basket-capacity`).val());
+    let userBasketCapacity = parseInt($(`#user-basket-capacity`).val());
+
+    let allBasket =  fillBaskets(nbBasket, basketCapacity, maxBallNumber);
+    allBasket.forEach((basket) => {
+      answer.append(`${basket}<br>`);
+    })
   }
 
   $(`#run-sim`).click(() => {
