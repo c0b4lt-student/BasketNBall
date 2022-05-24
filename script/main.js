794d0a1f627aa8b073dafe60600025d0b04a6f08
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-05-24 16:51:16 +0000

    adding id to input, get input in js

--- script/main.js
+++ script/main.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
-  $max_ball_number = 
-
+  let maxBallNumber = $(`#max-ball-number`);
+  let nbBasket = $(`#nb-basket`);
+  let basketCapacity = $(`#basket-capacity`);
+  let userBasketCapacity = $(`#user-basket-capacity`);
 
   function runSim() {
-    alert('sim is running');
+    alert(`${maxBallNumber.attr('value')}`);
   }
 
   $(`#run-sim`).click(() => {
