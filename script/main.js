1f0894c70e99fce2e5b2fa9c3f390c89e0ea26d8
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-05-25 12:43:45 +0000

    Simple soluce done. nt write documentations and unit test

--- script/main.js
+++ script/main.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-  let answer = $(`#answer`);
+  let answer = $(`#output`);
   let fullWin = $('#full-win');
   let singleWin = $('#single-win');
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  } //Genere un nombre aléatoire >= 0 et < max;
+  } //Genere un nombre aleatoire >= 0 et < max;
 
   function fillBasket(capacity, maxBall) {
     let load = getRandomInt(capacity) + 1;
@@ -20,20 +20,35 @@ $(document).ready(() => {
         basket.push(ball);
     }
     return basket.sort((a, b) => a - b);
-  } //Remplis un pannier avec au maximum 'capacity' balles;
+  } //Remplit un panier avec au maximum 'capacity' balles;
   function fillBaskets(nbBasket, basketCapacity, maxBallNumber) {
     let allBasket = [];
     for(let i = 0; i < nbBasket; i++) {
       allBasket.push(fillBasket(basketCapacity, maxBallNumber));
     }
     return allBasket;
-  } //Genere et retourne un tableau de panier remplis a l'aide de fillBasket;
+  } //Genere et retourne un tableau de paniers remplis a l'aide de fillBasket;
   function printAllBaskets(allBasket, userBasket) {
     allBasket.forEach((basket, i) => {
-      answer.append(`Pannier N°${i+1} : ${basket}<br>`);
+      answer.append(`Panier N°${i+1} : ${basket}<br>`);
     });
-    answer.append(`Pannier de l'utilisateur : ${userBasket}`);
-  } //Affiche la liste des panniers numérotés, dans une balise <p>
+    answer.append(`Panier de l'utilisateur : ${userBasket}`);
+  } //Affiche la liste des paniers numerotes, dans une balise <p>
+  function printWinningBasket(allBasket, userBasket) {
+    allBasket.forEach((basket, i) => {
+      let ballsCount = isBasketContainUserBalls(basket, userBasket);
+      if (ballsCount === 1) {
+        singleWin.append(`Panier n°${i + 1}, `);
+      }
+      if (ballsCount === basket.length) {
+        fullWin.append(`Panier n°${i + 1}, `);
+      }
+    });
+    if (singleWin.html().length > 0) //Clean output
+      singleWin.html(`${singleWin.html().slice(0, -2)}`);
+    if (fullWin.html().length > 0) //Clean output
+      fullWin.html(`${fullWin.html().slice(0, -2)}`);
+  } //Affiche la liste des paniers gagnants;
 
   function isBasketContainUserBalls(basket, userBasket) {
     let ballCounter = 0;
@@ -43,12 +58,12 @@ $(document).ready(() => {
         ballCounter++;
     });
     return ballCounter;
-  }
+  } //Compte combien de balles du panier "basket" sont presentes dans "userBasket";
 
-  function runSim() { //Executée par le click sur "lancer simulation";
+  function runSim() { //Executee par le clic sur "lancer simulation";
     answer.html('Résultats des tirages : <br>');
-    singleWin.html(`Panniers avec une seule balle présente dans le pannier de l'utilisateur : <br>`);
-    fullWin.html(`Panniers avec toutes les balles présentent dans le pannier de l'utilisateur : <br>`);
+    singleWin.html(`Paniers avec une seule balle présente dans le panier de l'utilisateur : <br>`);
+    fullWin.html(`Paniers avec toutes les balles présentes dans le panier de l'utilisateur : <br>`);
     let maxBallNumber = parseInt($(`#max-ball-number`).val());
     let nbBasket = parseInt($(`#nb-basket`).val());
     let basketCapacity = parseInt($(`#basket-capacity`).val());
@@ -57,13 +72,7 @@ $(document).ready(() => {
     let userBasket = fillBasket(userBasketCapacity, maxBallNumber);
 
     printAllBaskets(allBasket, userBasket);
-    allBasket.forEach((basket, i) => {
-      let ballsCount = isBasketContainUserBalls(basket, userBasket);
-      if (ballsCount === 1)
-        singleWin.append(`Pannier n°${i + 1}, `);
-      if (ballsCount === basket.length)
-        fullWin.append(`Pannier n°${i + 1}, `);
-    })
+    printWinningBasket(allBasket, userBasket);
   }
 
   $(`#run-sim`).click(() => {
