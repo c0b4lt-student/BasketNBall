502ec1f3fd476d4e3ac53ef0a9eb34edfa82821b
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-05-26 13:53:28 +0000

    add a test : is gri always return an int lesser than max

--- unit_test/getRandomInt.test.js
+++ unit_test/getRandomInt.test.js
@@ -1,4 +1,4 @@
-const getRandomInt = require('./getRandomInt');
+const getRandomInt = require('../script/getRandomInt');
 
 test('Get a random integer with max 0', () => {
     expect(getRandomInt(0)).toBe(0);
@@ -15,9 +15,20 @@ test('Get a random integer with max = -1', () => {
 
 test('Over 100 roll, test if getRandomInt return once max with max = 10', () => {
     let is_max_found = false;
+
     for (let i = 0; i < 100; i++) {
         if (getRandomInt(10) === 10)
             is_max_found = true;
     }
     expect(is_max_found).toBe(true);
-})
+});
+
+test('Over 100 rolls, test if getRandomInt ever return a number <= than max', () => {
+    let is_max_overtaked = false;
+
+    for (let i = 0; i < 100; i++) {
+        if (getRandomInt(10) > 10)
+            is_max_overtaked = true;
+    }
+    expect(is_max_overtaked).toBe(false);
+});
